a0b8e298aa71d39a6be3ac3ca667bd8ba3e82d9b
yeaminr07 <yeaminr07@gmail.com>
2023-06-21 20:52:31 +0600

    website-developMode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css'
-import Counter from './components/Counter'
-import Theme from './components/Theme'
+
 
 const App = () => {
     return (
         <div className='App'>
-            <Counter />
-            <br />
-            <hr />
-            <Theme />
+          Don yeamin
         </div>
     )
 }
